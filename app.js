4527861c51efaa5ca32faadaac4cb3aa0e2e1377
cajango-dev <cajango.dev@gmail.com>
2025-02-19 20:31:10 -0300

    feat: configurar servidor Express e integrar rotas e Swagger

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const animalRoutes = require('./src/routes/animalRoutes');
+const saudeRoutes = require('./src/routes/saudeRoutes');
+const piqueteRoutes = require('./src/routes/piqueteRoutes'); // Importar as rotas de piquetes
+const swaggerConfig = require('./src/swagger');
+
+const app = express();
+const PORT = 3000;
+
+// Middleware para JSON
+app.use(express.json());
+
+// Rotas
+app.use('/api', animalRoutes);
+app.use('/api', saudeRoutes);
+app.use('/api', piqueteRoutes); // Usar as rotas de piquetes
+
+// Swagger
+swaggerConfig(app);
+
+// Iniciar servidor
+app.listen(PORT, () => {
+console.log(`Servidor rodando em http://localhost:${PORT}`);
+console.log(`Documentação Swagger em http://localhost:${PORT}/api-docs`);
+});
