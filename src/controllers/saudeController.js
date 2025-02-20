08a535a8c30a72716bae895ec71c5f230af1b047
cajango-dev <cajango.dev@gmail.com>
2025-02-19 22:51:50 -0300

    feat: adicionado o peso no registro de saúde

--- src/controllers/saudeController.js
+++ src/controllers/saudeController.js
@@ -1,9 +1,9 @@
 const historicoSaude = []; // Simulando um "banco de dados" em memória
 
-// Registrar um novo tratamento ou vacina
+// Registrar um novo registro de saúde
 const registrarSaude = (req, res) => {
-const { animalId, tipo, descricao, data } = req.body;
-const registro = { animalId, tipo, descricao, data };
+const { animalId, tipo, descricao, data, peso, frequenciaCardiaca, temperatura, condicaoCorporal, observacoes } = req.body;
+const registro = { animalId, tipo, descricao, data, peso, frequenciaCardiaca, temperatura, condicaoCorporal, observacoes };
 historicoSaude.push(registro);
 res.status(201).json({ message: 'Registro de saúde adicionado!', registro });
 };
