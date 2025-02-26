c1b0397e60e6be1d6871eec33ef012e99393be09
cajango-dev <cajango.dev@gmail.com>
2025-02-26 20:32:47 -0300

    feat: configurar servidor Express e redirecionar para login.html

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const path = require('path');
+const logger = require('./src/config/logger')
+const authRoutes = require('./src/routes/authRoutes');
 const animalRoutes = require('./src/routes/animalRoutes');
 const saudeRoutes = require('./src/routes/saudeRoutes');
-const piqueteRoutes = require('./src/routes/piqueteRoutes'); // Importar as rotas de piquetes
+const piqueteRoutes = require('./src/routes/piqueteRoutes');
 const swaggerConfig = require('./src/swagger');
 
 const app = express();
@@ -11,7 +13,19 @@ const PORT = 3000;
 // Middleware para JSON
 app.use(express.json());
 
+// Middleware para logging de erros
+app.use((err, req, res, next) => {
+    logger.error(`${err.message}`);
+    res.status(500).json({ message: 'Erro interno no servidor' });
+});
+
+// Redirecionar para login.html ao acessar a raiz
+app.get('/', (req, res) => {
+    res.redirect('/login.html');
+});
+
 // Rotas
+app.use('/api', authRoutes); // Usar as rotas de autenticação
 app.use('/api', animalRoutes);
 app.use('/api', saudeRoutes);
 app.use('/api', piqueteRoutes); // Usar as rotas de piquetes
